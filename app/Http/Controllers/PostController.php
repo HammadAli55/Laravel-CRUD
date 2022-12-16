8841cc549e4303e9c9830ff2745e0e48bdf556f5
HammadAli55 <hammadalii@outlook.com>
2022-12-16 16:52:48 +0500

    posts are storing in mysql

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class PostController extends Controller
 {
-    //
+    public function addPost() {
+        return view('add-post');
+    }
+    public function savePost(Request $request) {
+       DB::table('posts')->insert([
+        'name' => $request -> name,
+        'description' => $request -> description
+       ]);
+
+       return back() -> with('post_add', 'Post added successfully');
+    }
 }
---
+++ resources/views/add-post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Post</title>
+</head>
+<body>
+    @if(Session::has ('post_add'))
+    <span>({Session::get(post_add)})</span>
+    @endif
+    <form method="post" action="{{route('save.post')}}">
+        @csrf
+        Post: <br><input type="text" name="name" value=""><br>
+        Description: <br><textarea name="description"></textarea><br>
+        <Input type="submit" value="submit"></Input>
+    </form>
+</body>
+</html>
